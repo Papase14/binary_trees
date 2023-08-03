3bb711b79b4b5baf776567167c44f8e120ebfd59
PatWork <tshupane@outlook.com>
2023-08-03 12:58:12 -0700

    0-binary_tree_node.c

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -4,21 +4,21 @@
  * @parent: A pointer to the parent node of the node to create.
  * @value: The value to put in the new node.
  *
- * Return: A pointer to the newly created node, 
+ * Return: A pointer to the newly created node,
  * or NULL if the allocation failed.
- */
+*/
 
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
-    binary_tree_t *new_node = malloc(sizeof(binary_tree_t));
+	binary_tree_t *new_node = malloc(sizeof(binary_tree_t));
 
-    if (new_node == NULL)
-        return (NULL);
+	if (new_node == NULL)
+		return (NULL);
 
-    new_node->n = value;
-    new_node->parent = parent;
-    new_node->left = NULL;
-    new_node->right = NULL;
+	new_node->n = value;
+	new_node->parent = parent;
+	new_node->left = NULL;
+	new_node->right = NULL;
 
-    return (new_node);
+	return (new_node);
 }
