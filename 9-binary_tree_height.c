46ba8ec0b5f92d184802778e9a8acb2cb1639c16
Tshupane Morake <40323964+Papase14@users.noreply.github.com>
2023-08-03 23:44:28 +0200

    9. Height✨

---
+++ 9-binary_tree_height.c
@@ -0,0 +1,18 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_height - Calculates the height of a binary tree.
+ * @tree: A pointer to the root node of the binary tree.
+ *
+ * Return: The height of the binary tree as a size_t value.
+ */
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	if (tree == NULL)
+		return (0);
+
+	size_t left_height = binary_tree_height(tree->left);
+	size_t right_height = binary_tree_height(tree->right);
+
+	return 1 + ((left_height > right_height) ? left_height : right_height);
+}
